57c4922e23bee0762d3d3ae238009a31cb25764f
Loki hornsby <lokihornsby@gmail.com>
2022-11-29 11:14:27 +0000

    Initial commit

---
+++ Assets/Player/Controls.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles all the controls for the player
+/// </summary>
+
+public class Controls : MonoBehaviour {
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
